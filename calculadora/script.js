b2068a9ade03caef2cb449972a0fb9610222e7e1
Ronan <ronan.martinscontato@gmail.com>
2025-12-04 15:40:30 -0300

    calculadora

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,21 +1,46 @@
-function insert(num)
-{
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
+var resultadoElement = document.getElementById('resultado');
+
+function insert(caracter) {
+    var expressaoAtual = resultadoElement.innerHTML;
+    resultadoElement.innerHTML = expressaoAtual + caracter;
 }
-function clean()
-{
-    document.getElementById('resultado').innerHTML = "";
-}
-function calcular()
-{
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado);
+
+function insertDot() {
+    var expressaoAtual = resultadoElement.innerHTML;
+    
+    if (expressaoAtual === "") {
+        resultadoElement.innerHTML = "0.";
+        return;
     }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "";
+    
+    var partes = expressaoAtual.split(/[\+\-\*\/]/g);
+    var ultimaParte = partes[partes.length - 1];
+    
+    if (ultimaParte.includes('.')) {
+        return;
+    }
+    
+    if (ultimaParte === "") {
+        resultadoElement.innerHTML += "0.";
+    } else {
+        resultadoElement.innerHTML += ".";
+    }
+}
+
+function clean() {
+    resultadoElement.innerHTML = "";
+}
+
+function calcular() {
+    var expressao = resultadoElement.innerHTML;
+    
+    if (expressao) {
+        try {
+            resultadoElement.innerHTML = eval(expressao);
+        } catch (e) {
+            resultadoElement.innerHTML = "Erro";
+        }
+    } else {
+        resultadoElement.innerHTML = "";
     }
 }
